a6ac77c8138615af1ea1a65094a5a40e1ca24310
juniosame <juniosame@hotmail.com>
2023-11-21 17:28:31 -0300

    Update usuario-atualiza.php

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -6,7 +6,7 @@ require_once "../inc/cabecalho-admin.php";
 $id = $_GET['id'];
 
 // Chamando a função e guardando o retorno dela
-$usuario = lerUmUsuario($conexao, $id)
+$usuario = lerUmUsuario($conexao, $id);
 
 
 // Verificando se o formulário foi acionadp
@@ -18,8 +18,19 @@ if(isset($_POST['atualizar'])){
 
 	/* Lógica para a senha
 	Se o campo senha estiver vazio OU se a senha digitada for igual à senha que já existe no banco de dados, então significa que o usuário NÃO ALTETOU A SENHA. Portanto, devemos MANTER a senha existente */
-
+	if(empty($_POST['senha']) ||
+		password_verify($_POST['senha'], $usuario['senha']) ) {
+		$senha = $usuario['senha']; // mantemos a mesma senha
+	} else {
 	/* Caso contrário, pegaremos a senha nova digitada e a codificamos antes de mandar para o banco */
+		$senha = password_hash($_POST['senha]'], PASSWORD_DEFAULT);
+	}
+
+	// Chamamos a função e passamos os dados
+	atualizarUsuario($conexao, $id, $nome, $email, $senha, $tipo);
+	
+	// Redirecionamos para a página de usúarios
+	header("location:usuarios.php")
 }
 ?>
 
